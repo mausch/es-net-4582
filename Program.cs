fdc67880d8937471c4f4f2d36b30df10b50efe30
Mauricio Scheffer <mauricioscheffer@gmail.com>
2020-04-16 21:55:06 +0100

    bug repro

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Nest;
+
+namespace es_bug_repro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var json = @"
+{
+  ""hits"": {
+  },
+  ""aggregations"":
+  {
+    ""some_agg"" : {
+      ""buckets"" : {
+        ""value1"" : {
+          ""doc_count"" : 0
+        },
+        ""value2"" : {
+          ""doc_count"" : 0
+        },
+        ""value3#something else"" : {
+          ""doc_count"" : 0
+        }
+      }
+    }
+  }
+}
+";
+            using var ms = new MemoryStream(Encoding.UTF8.GetBytes(json));
+            var response = new ElasticClient().RequestResponseSerializer
+              .Deserialize<SearchResponse<object>>(ms);
+            var filters = response.Aggregations
+              .Filters("some_agg")
+              .Select(x => x.Key)
+              .ToList();
+
+            const string expected = "value3#something else";
+            var actual = filters[2];
+            if (actual != expected)
+            { 
+                throw new Exception($"Expected '{expected}', actual '{actual}'");
+            }
+        }            
+    }
+}
